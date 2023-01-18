bff7bb6c900b1a7aaad6aff7a0e287660716c441
Luca Sinigaglia <lsinigag@e3r9p3.42roma.it>
2023-01-18 19:10:02 +0100

    l

--- lib_ft/ft_strrchr.c
+++ lib_ft/ft_strrchr.c
@@ -1,26 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*   ft_strrchar.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lsinigag <lsinigag@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/18 18:42:25 by lsinigag          #+#    #+#             */
-/*   Updated: 2023/01/18 19:02:18 by lsinigag         ###   ########.fr       */
+/*   Created: 2023/01/18 19:02:45 by lsinigag          #+#    #+#             */
+/*   Updated: 2023/01/18 19:07:46 by lsinigag         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*strchr(const char *str, int c)
+char	*strrchr(const char *str, int c)
 {
-	int	i;
+	size_t	i;
 
-	i = -1;
-	while (str[++i] != '\0')
+	i = 1 + ft_strlen(str);
+	while (i-- > 0)
 	{
 		if (str[i] == c)
-			return ((char *)(s + i));
+			return ((char *)(str + i));
 	}
 	return (0);
 }
